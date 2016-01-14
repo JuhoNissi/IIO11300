85ad5cdb3bebc5295ea12a60b203831cfaf72857
JuhoNissi <juho_356@hotmail.com>
2016-01-14 10:47:39 +0200

    Add BussinessLocigWindow class

---
+++ IIO11300Vktehtavat/Tehtava1/BLWindow.cs
@@ -0,0 +1,25 @@
+﻿/*
+* Copyright (C) JAMK/IT/Esa Salmikangas
+* This file is part of the IIO11300 course project.
+* Created: 12.1.2016 Modified: 14.1.2016
+* Authors: Juho Nissi ,Esa Salmikangas
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava1
+{
+    public class BusinessLogicWindow
+    {
+        /// <summary>
+        /// CalculatePerimeter calculates the perimeter of a window
+        /// </summary>
+        public static double CalculatePerimeter(double widht, double height)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
